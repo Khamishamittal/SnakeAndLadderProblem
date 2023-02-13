e658035cabb6820123463cc11ab51b0c6b1caf55
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-13 15:39:10 +0530

    Rolls the Dice to get a Random Number and check for option to get a desired position

--- RollsTheDice.cs
+++ RollsTheDice.cs
@@ -10,9 +10,36 @@ namespace SnakeAndLadderProblem
     {
         public static void GetANumber()
         {
-            Random random= new Random();
+            int position = 0;
+
+            Console.WriteLine("Welcome Player 1, Let's start the Game");
+            Console.WriteLine("Player 1 is at position : " + position);
+
+            Random random = new Random();
             int dice = random.Next(1, 7);
-            Console.WriteLine($"The player rolls a dice and gets: {dice}");    
+            Console.WriteLine($"The player rolls a dice and gets: {dice}");
+
+            int option = random.Next(0, 3);
+            if (option == 0)
+            {
+                Console.WriteLine("Player stays at the same position");
+
+
+            }
+            else if (option == 1)
+            {
+                Console.WriteLine("Player Moves Ahead by {0} from ladder", dice);
+                position += dice;
+
+
+            }
+            else
+            {
+                Console.WriteLine("Player Moves behind by {0} from ladder", dice);
+                position -= dice;
+
+            }
+            Console.WriteLine("Current Position is at : {0}", position);
         }
     }
 }
