a867763de555fe2866fcb6992dac47cb5d2e9e8c
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-13 14:58:11 +0530

    Rolls the Dice to get a Random Number

---
+++ RollsTheDice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class RollsTheDice
+    {
+        public static void GetANumber()
+        {
+            Random random= new Random();
+            int dice = random.Next(1, 7);
+            Console.WriteLine($"The player rolls a dice and gets: {dice}");    
+        }
+    }
+}
