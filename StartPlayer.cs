956b68441a2070b2f5660f9c8520a78f42ec2b67
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-13 14:30:49 +0530

    Lets start the game at position 0

---
+++ StartPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public  class StartPlayer
+    {
+        public static void StartGame()
+        {
+            int position = 0;
+            Console.WriteLine("Welcome player 1 , lets start the game");
+            Console.WriteLine("Player 1 is at position : " + position);
+        }
+        
+
+        }
+    }
+
